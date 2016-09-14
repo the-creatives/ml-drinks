1ee33516c700d27a434f9de2b071ef1a5cb08448
James W <james.wu@alumni.olin.edu>
2016-09-13 22:41:47 -0400

    add static folder for heroku

--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages'
+    'django.contrib.messages',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
