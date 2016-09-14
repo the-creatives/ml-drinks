5ca6603f1d6d165e9b97cec8de6cd0dd2d294613
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2016-09-14 16:42:10 -0400

    updated ui

--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'drinks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['drinks/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
