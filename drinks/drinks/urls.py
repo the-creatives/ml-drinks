932c6a7e370c7a9139b5d11d857bef76e36d8274
James W <james.wu@alumni.olin.edu>
2016-09-13 22:10:06 -0400

    GET endpoint which grabs the url as a path param

--- drinks/drinks/urls.py
+++ drinks/drinks/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^classify/$', views.classify, name='classify')
 ]
--- drinks/drinksapp/views.py
+++ drinks/drinksapp/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
+import indicoio
+import secrets
+from indicoio.custom import Collection
 
+indicoio.config.api_key = secrets.INDICO_KEY
+collection = Collection(secrets.INDICO_MODEL)
+
+def classify(request):
+	url = request.GET["url"]
+	return JsonResponse(collection.predict(url))
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
