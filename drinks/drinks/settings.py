b4051e06c2df65b25808b97ee5fadbca0a29b731
Lucy Wilcox <lucy.wilcox@students.olin.edu>
2016-09-14 16:38:45 -0400

    changes pre pull

--- drinks/drinks/settings.py
+++ drinks/drinks/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'drinks.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['drinks/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
